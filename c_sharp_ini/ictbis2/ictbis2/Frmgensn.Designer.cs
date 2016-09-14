4f8ec425cee512a9ff3a8c570e8a4ba636edaead
kook01 <somphop.kk@gmail.com>
2016-09-14 13:50:09 +0700

    c_ini
    c_ini

---
+++ c_sharp_ini/ictbis2/ictbis2/Frmgensn.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ictbis2
+{
+    partial class frmgensn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnchkgenini = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFileTxt = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtboxexpresssn = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtboxdataencrpt = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtboxdatadecrpt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutICTBISToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnchkgenini
+            // 
+            this.btnchkgenini.Location = new System.Drawing.Point(759, 45);
+            this.btnchkgenini.Name = "btnchkgenini";
+            this.btnchkgenini.Size = new System.Drawing.Size(149, 29);
+            this.btnchkgenini.TabIndex = 0;
+            this.btnchkgenini.Text = "1. Check and Gen file ini";
+            this.btnchkgenini.UseVisualStyleBackColor = true;
+            this.btnchkgenini.Click += new System.EventHandler(this.btnchkgenini_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data Decrpt";
+            // 
+            // btnFileTxt
+            // 
+            this.btnFileTxt.Location = new System.Drawing.Point(759, 89);
+            this.btnFileTxt.Name = "btnFileTxt";
+            this.btnFileTxt.Size = new System.Drawing.Size(184, 29);
+            this.btnFileTxt.TabIndex = 2;
+            this.btnFileTxt.Text = "2. Get File txt and replace sTring";
+            this.btnFileTxt.UseVisualStyleBackColor = true;
+            this.btnFileTxt.Click += new System.EventHandler(this.btnFileTxt_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(759, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(759, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtboxexpresssn
+            // 
+            this.txtboxexpresssn.Enabled = false;
+            this.txtboxexpresssn.Location = new System.Drawing.Point(12, 45);
+            this.txtboxexpresssn.Name = "txtboxexpresssn";
+            this.txtboxexpresssn.Size = new System.Drawing.Size(353, 20);
+            this.txtboxexpresssn.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Encrpt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Express SN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Data Encrpt";
+            // 
+            // txtboxdataencrpt
+            // 
+            this.txtboxdataencrpt.Enabled = false;
+            this.txtboxdataencrpt.Location = new System.Drawing.Point(12, 93);
+            this.txtboxdataencrpt.Name = "txtboxdataencrpt";
+            this.txtboxdataencrpt.Size = new System.Drawing.Size(350, 20);
+            this.txtboxdataencrpt.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(371, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Decrpt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtboxdatadecrpt
+            // 
+            this.txtboxdatadecrpt.Enabled = false;
+            this.txtboxdatadecrpt.Location = new System.Drawing.Point(12, 141);
+            this.txtboxdatadecrpt.Name = "txtboxdatadecrpt";
+            this.txtboxdatadecrpt.Size = new System.Drawing.Size(347, 20);
+            this.txtboxdatadecrpt.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(25, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 12;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(470, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutICTBISToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutICTBISToolStripMenuItem
+            // 
+            this.aboutICTBISToolStripMenuItem.Name = "aboutICTBISToolStripMenuItem";
+            this.aboutICTBISToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.aboutICTBISToolStripMenuItem.Text = "About ICTBIS";
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(374, 160);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(71, 36);
+            this.btnexit.TabIndex = 14;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // frmgensn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 202);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtboxdatadecrpt);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtboxdataencrpt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtboxexpresssn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFileTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnchkgenini);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmgensn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Gen SN";
+            this.Load += new System.EventHandler(this.frmgensn_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnchkgenini;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFileTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtboxexpresssn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtboxdataencrpt;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtboxdatadecrpt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutICTBISToolStripMenuItem;
+        private System.Windows.Forms.Button btnexit;
+    }
+}
+
---
+++ c_sharp_ini/ictbis2/ictbis2/Frmgensn.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace ictbis2
+{
+    public partial class frmgensn : Form
+    {
+        classini clsini = new classini();
+        encryptsn ensn = new encryptsn();
+        decryptsn desn = new decryptsn();
+        public frmgensn()
+        {
+            InitializeComponent();
+            showdatafirst();
+
+        }
+
+        private void showdatafirst()
+        {
+            string[] dataexpress = null;
+            dataexpress = clsini.readdatatxtfile();
+            txtboxexpresssn.Text = dataexpress[0];
+
+            if (clsini.IniWriteValue("SN_Express", "SN", dataexpress[0]))
+            {
+              //
+            }
+            else
+            {
+                label4.Text = "ไม่สามารถเพิ่มข้อมูลได้";
+            }
+        }
+
+        private void btnchkgenini_Click(object sender, EventArgs e)
+        {
+            classini clsini = new classini();
+            string[] abc = new string[3];
+            abc = clsini.readdatatxtfile();
+            Debug.Print("data 1 is " + abc[0]);
+            Debug.Print("data 2 is " + abc[1]);
+            Debug.Print("data 3 is " + abc[2]);
+        }
+
+        private void btnFileTxt_Click(object sender, EventArgs e)
+        {
+            encryptsn ensn = new encryptsn();
+            Debug.Print("data 1 is " + ensn.sn1);
+            Debug.Print("data 2 is " + ensn.sn2);
+            Debug.Print("data 3 is " + ensn.sn3);
+            ensn.splitdt();
+            Debug.Print("data year(Hex) is " + ensn.hexyear);
+            Debug.Print("data month(Hex) is " + ensn.hexmonth);
+            int abc = ensn.chgdec(ensn.hexyear);
+            Debug.Print("data back to year(Decimal) is " + abc);
+
+            Debug.Print("Data Encrpt is " + ensn.EncrptData());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //W10E028915
+            string tmp = "081WE7E0RUX95211099";
+            int abc = tmp.Length;
+            Debug.Print("length is " + abc);
+            string[] splittmp = null;
+
+
+            splittmp = new string[abc];
+            //splittmp[1] = tmp.Substring(5, 3); //Year
+            //splittmp[2] = tmp.Substring(11, 1); //Month
+            //splittmp[3] = tmp.Substring(17, 1); //Month
+            //Debug.Print("data year of " + tmp + " is " + splittmp[1]);
+            //Debug.Print("data month1 of " + tmp + " is " + splittmp[2]);
+            //Debug.Print("data month2 of " + tmp + " is " + splittmp[3]);
+
+            //splittmp[0] = tmp.Substring(0, 1);
+            //splittmp[1] = tmp.Substring(1, 1);
+            //splittmp[2] = tmp.Substring(2, 1);
+            //splittmp[3] = tmp.Substring(3, 1);
+            //splittmp[4] = tmp.Substring(4, 1);
+            //splittmp[5] = tmp.Substring(5, 1);
+            //splittmp[6] = tmp.Substring(6, 1);
+            //splittmp[7] = tmp.Substring(7, 1);
+            //splittmp[8] = tmp.Substring(8, 1);
+            //splittmp[9] = tmp.Substring(9, 1);
+            for (int i = 0; i < abc; i++)
+            {
+                splittmp[i] = tmp.Substring(i, 1);
+                Debug.Print("order:" + i + " data of " + tmp + " is " + splittmp[i]);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //  encryptsn ensn = new encryptsn();
+            //  decryptsn desn = new decryptsn();
+            ensn.splitdt();
+            Debug.Print("Data original is W-10E-028915 ");
+            Debug.Print("Data Encrpt is " + ensn.EncrptData());
+            desn.Showdataback();
+            Debug.Print("Data back to original is " + desn.sn1);
+            desn.mergeData4YearMonth();
+            Debug.Print("Data year is " + desn.dt_years + " and month is " + desn.dt_months);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ensn.splitdt();
+            txtboxdataencrpt.Text = ensn.EncrptData();
+            if (clsini.IniWriteValue("SN_ictbis", "SN", ensn.EncrptData()))
+            {
+                //
+            }
+            else
+            {
+                label4.Text = "**** ไม่สามารถเพิ่มข้อมูลได้";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            desn.Showdataback();
+            txtboxdatadecrpt.Text = desn.sn1;
+        }
+
+        private void frmgensn_Load(object sender, EventArgs e)
+        {
+            this.MaximumSize = new Size(486,224);
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit application?", "ออกจากโปรแกรม", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                Application.Exit(); //comment
+            }
+            else
+            {
+            }
+        }
+    }
+}
